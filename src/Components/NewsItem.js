f174f28f4144417bb88abc762fd2ec3daa719776
jmv72001 <jmv72001@gmail.com>
2025-03-17 18:44:25 +0400

    newsApp project

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+export default class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
+    return (
+      <div className="my-3">
+        <div className="card" style={{ width: "18rem" }}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
+          >
+            <span className="badge rounded-pill bg-danger"> {source} </span>
+          </div>
+          <img
+            className="card-img-top"
+            src={
+              imageUrl
+                ? imageUrl
+                : "https://static.seekingalpha.com/cdn/s3/uploads/getty_images/1255079689/image_1255079689.jpg?io=getty-c-w750"
+            }
+            alt="Card-image cap"
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small class="text-muted">
+                By {!author ? "unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
